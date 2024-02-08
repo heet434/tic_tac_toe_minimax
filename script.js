bc7a70f8a5c29a69a5081107e954ee1d8ffd3acd
Heet Patel <hiheet04@gmail.com>
2024-02-08 14:16:51 +0530

    Implemented minimax algorithm for the game

--- script.js
+++ script.js
@@ -1,5 +1,293 @@
-addMark = function(){
-    if(pmove=='o'){
-        document.getElementById()
+var board = [
+    ['','',''],
+    ['','',''],
+    ['','','']
+];
+var HUMAN = -1;
+var AI = 1;
+
+//var currentPlayer = HUMAN;
+var winner = null;
+var winningIndexes = [];
+
+var prevMove = 'x';
+var currentMove = 'o';
+let maxdepth = 9;
+
+function addMark(id){
+    let x = Number(id[0]);
+    let y = Number(id[1]);
+    if(board[x][y]==''){
+        board[x][y] = currentMove;
+        prevMove = currentMove;
+        document.getElementById(id).style.backgroundImage = `url("${currentMove}.png")`;
+        if(currentMove=='x'){
+            currentMove = 'o';
+        }else{
+            currentMove = 'x';
+        }
+        if(isGameOver()){
+            displayWinner();
+        }
     }
+}
+
+function hasWinner(){
+    //checking rows
+    for(let i=0;i<3;i++){
+        if(board[i][0]!='' && board[i][0]==board[i][1] && board[i][1]==board[i][2]){
+            winningIndexes = [[i,0],[i,1],[i,2]];
+            return true;
+        }
+    }
+    //checking columns
+    for(let i=0;i<3;i++){
+        if(board[0][i]!='' && board[0][i]==board[1][i] && board[1][i]==board[2][i]){
+            winningIndexes = [[0,i],[1,i],[2,i]];
+            return true;
+        }
+    }
+    //checking first diagonal
+    if(board[0][0]!='' && board[0][0]==board[1][1] && board[1][1]==board[2][2]){
+        winningIndexes = [[0,0],[1,1],[2,2]];
+        return true;
+    }
+    //checking second diagonal
+    if(board[0][2]!='' && board[0][2]==board[1][1] && board[1][1]==board[2][0]){
+        winningIndexes = [[0,2],[1,1],[2,0]];
+        return true;
+    }
+    return false;
+}
+
+function isBoardFull(){
+    for(let i=0;i<3;i++){
+        for(let j=0;j<3;j++){
+            if(board[i][j]==''){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function isGameOver(){
+    if(hasWinner()){
+        winner = prevMove;
+        return true;
+    }else if(isBoardFull()){
+        winner = 'tie';
+        return true;
+    }
+    return false;
+}
+
+function displayWinner(){
+    if(winner!='tie'){
+        for(let i=0;i<3;i++){
+            for(let j=0;j<3;j++){
+                for(let k=0;k<3; k++){
+                    if(winningIndexes[k][0]==i && winningIndexes[k][1]==j){
+                        document.getElementById(`${i}${j}`).style.backgroundImage = 'none';
+                        document.getElementById(`${i}${j}`).style.backgroundColor = 'green';
+                    }
+                }
+            }
+        }
+        document.getElementById('winnertext').innerText = `Winner: ${winner.toUpperCase()}`;
+        document.getElementById('winner').style.display= 'block';
+        document.getElementById('add_ai_turn').style.display = 'none';
+    }
+    else {
+        document.getElementById('winnertext').innerText = `It's a tie!`;
+        document.getElementById('winner').style.display= 'block';
+        document.getElementById('add_ai_turn').style.display = 'none';
+    }
+}
+
+function ai_turn(){
+    if(winner==null){
+        var currentSign = currentMove;
+        let numEmpty = 0;
+        for(let i=0;i<3;i++){
+            for(let j=0;j<3;j++){
+                if(board[i][j]==''){
+                    numEmpty++;
+                }
+            }
+        } 
+        var nextTurn = null;
+        if(numEmpty==9){
+            nextTurn = [Math.floor(Math.random()*3), Math.floor(Math.random()*3), board];
+        }
+        else{
+            nextTurn = minimax(board, maxdepth ,'comp', currentMove, prevMove);
+        }
+        // if (currentSign=='x'){
+        //     var nextTurn = minimax(board, maxdepth ,'max');
+        // }else{
+        //     var nextTurn = minimax(board, maxdepth ,'min');
+        // }
+        //console.log(nextTurn);
+        var x = nextTurn[0];
+        var y = nextTurn[1];
+        board[x][y] = currentSign;
+        prevMove = currentSign;
+        document.getElementById(`${x}${y}`).style.backgroundImage = `url("${currentSign}.png")`;
+        currentMove = currentMove=='x'?'o':'x';
+        if(isGameOver()){
+            displayWinner();
+        }
+    }
+}
+//var num =1000;
+function minimax(board, depth, player, currentMove, prevMove){
+    var best = null;
+    
+    var newBoard = board;
+    if(player=='comp'){
+        best = [-1,-1,-Infinity];
+    }else{
+        best = [-1,-1,Infinity];
+    }
+    if(depth==0 || terminal(newBoard)){
+        if(player=='comp'){
+            best = [-1,-1,eval(newBoard, currentMove, prevMove)];
+            return best;
+        }else{
+            best = [-1,-1,eval(newBoard, prevMove, currentMove)];
+            return best;
+        }
+    };
+    if(player=='comp'){
+        for(let i=0;i<3;i++){
+            for(let j=0;j<3;j++){
+                if(newBoard[i][j]==''){
+                    tempBoard = newBoard;
+                    tempBoard[i][j] = currentMove;
+                    var value = minimax(tempBoard, depth-1, 'human', prevMove, currentMove);
+                    //console.log(value)
+                    newBoard[i][j] = '';
+                    if(value[2]>best[2]){
+                        best = [i,j,value[2]];
+                        //if(num<20)console.log(best);
+                        //num--;
+                    }
+                }
+            }
+        }
+    }else if(player=='human'){
+        for(let i=0;i<3;i++){
+            for(let j=0;j<3;j++){
+                if(newBoard[i][j]==''){
+                    // newBoard[i][j] = currentMove;
+                    //tempBoard = newBoard.map((item)=>item.slice());
+                    tempBoard = newBoard;
+                    tempBoard[i][j] = currentMove;
+                    var value = minimax(tempBoard, depth-1, 'comp', prevMove, currentMove);
+                    //console.log(value)
+                    newBoard[i][j] = '';
+                    if(value[2]<best[2]){
+                        best = [i,j,value[2]];
+                        //if(num<20)console.log(best);
+                        //num--;
+                    }
+                }
+            }
+        }
+    }
+    return best;
+}
+
+let terminal= function(board){
+    //checking rows and columns
+    for(let i=0;i<3;i++){
+        if(board[i][0]!='' && board[i][0]==board[i][1] && board[i][1]==board[i][2]){
+            return true;
+        }
+        if(board[0][i]!='' && board[0][i]==board[1][i] && board[1][i]==board[2][i]){
+            return true;
+        }
+    }
+    //checking diagonals
+    if(board[0][0]!='' && board[0][0]==board[1][1] && board[1][1]==board[2][2]){
+        return true;
+    }
+    if(board[0][2]!='' && board[0][2]==board[1][1] && board[1][1]==board[2][0]){
+        return true;
+    }
+    //checking if board is full
+    for(let i=0;i<3;i++){
+        for(let j=0;j<3;j++){
+            if(board[i][j]==''){
+                return false;
+            }
+        }
+    }
+    return true;
+
+};
+
+let eval = function(board, currentPlayer, prevPlayer){
+    //console.log(1);
+    let score = 0;
+    var opponent = prevPlayer;
+    //adding number of directions open for max to win
+    for(let i=0;i<3;i++){
+        if((board[i][0]==currentPlayer||board[i][0]=='') && (board[i][1]==currentPlayer||board[i][1]=='') && (board[i][2]==currentPlayer||board[i][2]=='')){
+            score++;
+        } //checking rows
+        if((board[0][i]==currentPlayer||board[0][i]=='') && (board[1][i]==currentPlayer||board[1][i]=='') && (board[2][i]==currentPlayer||board[2][i]=='')){
+            score++;
+        } //checking columns
+    }
+        if((board[0][0]==currentPlayer||board[0][0]=='') && (board[1][1]==currentPlayer||board[1][1]=='') && (board[2][2]==currentPlayer||board[2][2]=='')){
+            score++;
+        } //checking first diagonal
+        if((board[0][2]==currentPlayer||board[0][2]=='') && (board[1][1]==currentPlayer||board[1][1]=='') && (board[2][0]==currentPlayer||board[2][0]=='')){
+            score++;
+        } //checking second diagonal
+    //subtracting number of directions open for min to win
+    for(let i=0;i<3;i++){
+        if((board[i][0]==opponent||board[i][0]=='') && (board[i][1]==opponent||board[i][1]=='') && (board[i][2]==opponent||board[i][2]=='')){
+            score--;
+        } //checking rows
+        if((board[0][i]==opponent||board[0][i]=='') && (board[1][i]==opponent||board[1][i]=='') && (board[2][i]==opponent||board[2][i]=='')){
+            score--;
+        } //checking columns
+    }
+        if((board[0][0]==opponent||board[0][0]=='') && (board[1][1]==opponent||board[1][1]=='') && (board[2][2]==opponent||board[2][2]=='')){
+            score--;
+        } //checking first diagonal
+        if((board[0][2]==opponent||board[0][2]=='') && (board[1][1]==opponent||board[1][1]=='') && (board[2][0]==opponent||board[2][0]=='')){
+            score--;
+        } //checking second diagonal
+        //checking if there is a winner
+        let tienum = 0;
+        for (let i=0;i<3;i++){
+            for(let j=0;j<3;j++){
+                if(board[i][j]!=''){
+                    tienum++;
+                }
+            }
+        }
+        if(tienum==9){
+            score = 0;
+        }
+        for(let i=0;i<3;i++){
+            if(board[i][0]!='' && board[i][0]==board[i][1] && board[i][1]==board[i][2]){
+                board[i][0]==currentPlayer?score=Infinity:score=-Infinity;
+            }
+            if(board[0][i]!='' && board[0][i]==board[1][i] && board[1][i]==board[2][i]){
+                board[0][i]==currentPlayer?score=Infinity:score=-Infinity;
+            }
+        }
+        if(board[0][0]!='' && board[0][0]==board[1][1] && board[1][1]==board[2][2]){
+            board[0][0]==currentPlayer?score=Infinity:score=-Infinity;
+        }
+        if(board[0][2]!='' && board[0][2]==board[1][1] && board[1][1]==board[2][0]){
+            board[0][2]==currentPlayer?score=Infinity:score=-Infinity;
+        }
+        //checkig if there is a tie
+    return score;
 }
